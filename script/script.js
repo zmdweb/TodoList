9a13250317ae9ee7e3c1a1c8e11a6092d364a9f8
zmdweb <zmdwebagency@gmail.com>
2021-03-01 00:11:58 +0300

    Add html,css and js file. Error: QuarySellector

---
+++ script/script.js
@@ -0,0 +1,44 @@
+let AddMessage = document.querySelector('.message'),
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo');
+
+
+    let todoList =[];
+
+    if (localStorage.getItem('todo')) {
+        todoList = JSON.parse(localStorage.getItem('todo'));
+        displayMessages();
+    }
+
+    addButton.addEventListener('click',function(){
+        let newTodo = {
+            todo: AddMessage.value,
+            checked: false,
+            important: false
+        };
+
+        todoList.push(newTodo);
+        displayMessages();
+        localStorage.setItem('todo',JSON.stringify(todoList));
+    })
+
+    
+    function displayMessages(){
+        let displayMessage = ``;
+        todoList.forEach(function(item,i){
+            displayMessage += `
+            <li>
+                <input type='checkbox' id='item_${i} ${item.checked ? 'checked' : ''}>
+                <label for='item_${i}'>${item.todo}</label>
+            </li>
+            `;
+            todo.innerHTML = displayMessage;
+        });
+    }
+
+    todo.addEventListener('change',function(event){
+            let idInput = event.target.getAttribute('id');
+            let forLabel = todo.querySelector('[for='+ idInput + ']');
+            console.log('forLabel',forLabel);
+    });
+
